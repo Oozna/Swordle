fc4874de5119d65bc0a4de472878cdd0d49f3a60
Oozna <johanssonericwilliam@gmail.com>
2022-02-27 10:11:59 +0100

    added divs for the squares

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
+"use strict"
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	createSquares();
 
 	function createSquares() {
-		const gameBoard = document.querySelector('board');
+		const gameBoard = document.getElementById("board");
 		for (let index = 0; index < 6 * 5; index++) {
 			let square = document.createElement("div");
-			square.classList.add('square');
-			square.setAttribute('id', index);
+			square.classList.add("square");
+			square.setAttribute("id", index);
 			gameBoard.appendChild(square);
 
 		}
 	}
-
-
-
-
-
-
-
 });
